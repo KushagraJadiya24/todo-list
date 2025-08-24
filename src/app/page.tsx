610939ae9b5fe5626edd19c7d77c98d76dc867d3
Kushagra Jadiya <kjadiya2004@gmail.com>
2025-08-24 17:11:13 +0530

    Working Todo List Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,16 @@ import { TodoItem } from "@/components/TodoItem";
 function getTodos(){
   return prisma.todo.findMany()
 }
+
+async function toggleTodo(id:string,complete:boolean){
+"use server"
+
+await prisma.todo.update({where: {id}, data:{complete}})
+}
 export default async function Home(){
 
   const todos = await getTodos()
-  // await prisma.todo.create({data:{title:"test", complete:false}})
+  // await prisma.todo.deleteMany()
 
   return (
     <>
@@ -19,7 +25,7 @@ export default async function Home(){
     </header>
     <ul className="pl-4">
       {todos.map(todo=>(
-        <TodoItem key={todo.id} {...todo} />
+        <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo}/>
       ))}
     </ul>
     </>
